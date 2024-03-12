ea1ffb13f4329b53d1707c0dd33b7c190c4db857
Claudio9616 <clares9611@gmail.com>
2024-03-12 15:42:30 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+@section('table-content')
+<ul>
+    <li>creare database e file train nei model</li>
+    <li>collegare questo file nel controller e collegare questo con il file web</li>
+    <li>creare il file migration per creare la tebella e le colonne con i relativi tipi</li>
+    <li>creare file factory per inserire record falsi</li>
+</ul>
+@endsection
+ 
